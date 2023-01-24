ff6e084709149a0b2bdfc697faf433d9e859402d
s6352410016 <s6352410016@sau.ac.th>
2023-01-24 16:22:16 +0700

    24/01/2023 16.22

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const router = require('./routes/router');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname , './client/build')));
+app.get('*' , (req , res) => {
+    res.sendFile(path.join(__dirname , './client/build/index.html'));
+});
+app.use(router);
+
+app.listen(PORT , () => {
+    console.log('starting server...');
+});
