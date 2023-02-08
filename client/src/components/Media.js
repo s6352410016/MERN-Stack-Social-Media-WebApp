d10601c848c84edfed20a6cf8a49645a36b66524
s6352410016 <s6352410016@sau.ac.th>
2023-02-08 21:54:31 +0700

    08/02/2023 21.54

--- client/src/components/Media.js
+++ client/src/components/Media.js
@@ -262,11 +262,13 @@ const Media = () => {
         <div className='content-center-in-body'>
           <div className='create-post-container'>
             <div className='container-post-content'>
-              <div className='box-of-user-profile-img'>
-                <div className='container-user-profile-img'>
-                  <img src={require('../images/allUserProfileImg/user1.png')}/>
+              <Link to='/profile' className='link-to-profile-post'>
+                <div className='box-of-user-profile-img'>
+                  <div className='container-user-profile-img'>
+                    <img src={require('../images/allUserProfileImg/user1.png')}/>
+                  </div>
                 </div>
-              </div>
+              </Link>
               <div className='container-input-post'>
                 <input type='text' placeholder="what's happening"/>
               </div>
@@ -274,15 +276,17 @@ const Media = () => {
             <div className='container-icons-post'>
               <div className='image-upload-icon'>
                 <label for='image'>
-                  <FontAwesomeIcon icon={faImage}/>
-                  <input type='file' id='image'/> 
+                  <FontAwesomeIcon icon={faImage} className='style-icon-post' id='color-icon-image'/>
+                  <input type='file' id='image' className='display-none-input-file'/> 
                 </label>
+                <p>Photo</p>
               </div>
               <div className='video-upload-icon'>
                 <label for='video'>
-                  <FontAwesomeIcon icon={faCirclePlay}/>
-                  <input type='file' id='video'/> 
+                  <FontAwesomeIcon icon={faCirclePlay} className='style-icon-post' id='color-icon-video'/>
+                  <input type='file' id='video' className='display-none-input-file'/> 
                 </label>
+                <p>Video</p>
               </div>
               <div className='post-button-container'>
                 <button>Post</button>
