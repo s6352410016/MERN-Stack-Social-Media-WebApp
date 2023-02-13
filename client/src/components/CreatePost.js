bfb9c93d936527ba915944465520e111e558fc12
s6352410016 <s6352410016@sau.ac.th>
2023-02-13 18:31:50 +0700

    13/02/2023 18.31

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faImage, faCirclePlay, faFaceGrinBeam } from '@fortawesome/free-solid-svg-icons';
+import { faImage, faCirclePlay, faFaceGrinBeam , faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { useRef, useState } from 'react';
 import EmojiPicker from 'emoji-picker-react';
 
@@ -12,10 +12,30 @@ const CreatePost = () => {
 
   const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
   const [postMsg, setPostMsg] = useState('');
+  const [cursorPosition, setCursorPosition] = useState();
+  const [previewImg, setPreviewImg] = useState([]);
+  const [openPreviewImg, setOpenPreviewImg] = useState(false);
 
-  const emojiClick = ({emoji}) => {
-    console.log(emoji);
+  const emojiClick = ({ emoji }) => {
+    inputRef.current.focus();
+    const start = postMsg.substring(0, inputRef.current.selectionStart);
+    const end = postMsg.substring(inputRef.current.selectionStart);
+    const msg = start + emoji + end;
+    setPostMsg(msg);
+    setCursorPosition(start.length + emoji.length);
   }
+
+  const imagePreview = (e) => {
+    const files = e.target.files;
+    const arrFiles = Array.from(files);
+    const createUrlFromFiles = arrFiles.map((files) => URL.createObjectURL(files));
+    setPreviewImg(createUrlFromFiles);
+    setOpenPreviewImg(true);
+  }
+
+  useEffect(() => {
+    inputRef.current.selectionEnd = cursorPosition;
+  }, [cursorPosition]);
 
   return (
     <div className='container-content-center-in-body'>
@@ -29,36 +49,48 @@ const CreatePost = () => {
             </div>
           </Link>
           <div className='container-input-post'>
-            <input type='text' placeholder="what's happening" ref={inputRef} onChange={(e) => setPostMsg(e.target.value)} />
+            <input id='auto-focus' type='text' placeholder="what's happening" value={postMsg} ref={inputRef} onChange={(e) => setPostMsg(e.target.value)} />
           </div>
         </div>
         <div className='container-icons-post'>
-          <div className='image-upload-icon' onClick={() => imageIconRef.current.click()}>
-            <FontAwesomeIcon icon={faImage} className='style-icon-post' id='color-icon-image' />
-            <input type='file' multiple className='display-none-input-file' ref={imageIconRef} />
-            <p>Photo</p>
-          </div>
-          <div className='video-upload-icon' onClick={() => videoIconRef.current.click()}>
-            <FontAwesomeIcon icon={faCirclePlay} className='style-icon-post' id='color-icon-video' />
-            <input type='file' className='display-none-input-file' ref={videoIconRef} />
-            <p>Video</p>
-          </div>
-          <div className='emoji-text-container'>
-            {openEmojiPicker &&
-              <>
-                <div className='background-emoji-text-container' onClick={() => setOpenEmojiPicker(false)}></div>
-                <EmojiPicker onEmojiClick={emojiClick}/>
-              </>
-            }
-            <div className='emoji-onclick' onClick={() => setOpenEmojiPicker(!openEmojiPicker)}>
-              <FontAwesomeIcon icon={faFaceGrinBeam} className='style-icon-post' id='color-icon-emoji' />
-              <p>Emoji</p>
+          <form encType='multipart/form-data' className='form-fix-style-in-container-icons-post'>
+            <div className='image-upload-icon' onClick={() => imageIconRef.current.click()}>
+              <FontAwesomeIcon icon={faImage} className='style-icon-post' id='color-icon-image' />
+              <input type='file' multiple accept='image/png , image/jpeg , image/webp' className='display-none-input-file' ref={imageIconRef} onChange={(e) => imagePreview(e)} />
+              <p>Photo</p>
             </div>
-          </div>
-          <div className='post-button-container'>
-            <button>Post</button>
+            <div className='video-upload-icon' onClick={() => videoIconRef.current.click()}>
+              <FontAwesomeIcon icon={faCirclePlay} className='style-icon-post' id='color-icon-video' />
+              <input type='file' className='display-none-input-file' ref={videoIconRef} />
+              <p>Video</p>
+            </div>
+            <div className='emoji-text-container'>
+              {openEmojiPicker &&
+                <>
+                  <div className='background-emoji-text-container' onClick={() => setOpenEmojiPicker(false)}></div>
+                  <EmojiPicker onEmojiClick={emojiClick} />
+                </>
+              }
+              <div className='emoji-onclick' onClick={() => setOpenEmojiPicker(!openEmojiPicker)}>
+                <FontAwesomeIcon icon={faFaceGrinBeam} className='style-icon-post' id='color-icon-emoji' />
+                <p>Emoji</p>
+              </div>
+            </div>
+            <div className='post-button-container'>
+              <button>Post</button>
+            </div>
+          </form>
+        </div>
+        {openPreviewImg &&
+        <div className='container-of-preview-img'>
+          <FontAwesomeIcon icon={faCircleXmark} className='style-xmark' onClick={() => setOpenPreviewImg(false)}/>
+          <div className='container-img-in-preview'>
+            {previewImg.map((files, index) => (
+              <img src={files} key={index} alt='previewImg' />
+            ))}
           </div>
         </div>
+      }
       </div>
     </div>
   );
--- client/src/components/PeopleYouMayKnow.js
+++ client/src/components/PeopleYouMayKnow.js
@@ -1,30 +1,47 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faUserPlus} from '@fortawesome/free-solid-svg-icons';
+import { faUserPlus, faUserCheck } from '@fortawesome/free-solid-svg-icons';
+import { RotatingLines } from 'react-loader-spinner';
 
-const PeopleYouMayKnow = ({image , fullname}) => {
-  return (
-    <div className='container-people-you-may-know'>
-        <Link to='/id' className='text-decoration-none-in-container-people-you-may-know' id='fix-container-img'>
-            <div className='container-image-people-you-may-know'>
-                <div className='img-container-people-you-may-know'>
-                    <img src={image} alt='imageForUser'/>
-                </div>          
+const PeopleYouMayKnow = ({ image, fullname }) => {
+    const [changeIcon, setChangeIcon] = useState(false);
+    const [loadingEffectFollow, setLoadingEffectFollow] = useState(false);
+
+    const followUser = () => {
+        setLoadingEffectFollow(true);
+        setTimeout(() => {
+            setLoadingEffectFollow(false);
+            setChangeIcon(true);
+        }, 1000);
+    }
+
+    return (
+        <div className='container-people-you-may-know'>
+            <Link to='/id' className='text-decoration-none-in-container-people-you-may-know' id='fix-container-img'>
+                <div className='container-image-people-you-may-know'>
+                    <div className='img-container-people-you-may-know'>
+                        <img src={image} alt='imageForUser' />
+                    </div>
+                </div>
+            </Link>
+            <Link to='/id' className='text-decoration-none-in-container-people-you-may-know' id='fix-container-fullname'>
+                <div className='container-fullname-people-you-may-know'>
+                    <b>{fullname}</b>
+                </div>
+            </Link>
+            <div className='container-button-follow-people-you-may-know'>
+                <button onClick={followUser}>
+                    {loadingEffectFollow
+                        ?
+                        <RotatingLines strokeColor="#B9B9B9" strokeWidth="5" animationDuration=".8" width="50%" visible={true} />
+                        :
+                        <FontAwesomeIcon icon={changeIcon ? faUserCheck : faUserPlus} className='follower-user-icon' />
+                    }
+                </button>
             </div>
-        </Link>
-        <Link to='/id' className='text-decoration-none-in-container-people-you-may-know' id='fix-container-fullname'>
-            <div className='container-fullname-people-you-may-know'>
-                <b>{fullname}</b>
-            </div>
-        </Link>
-        <div className='container-button-follow-people-you-may-know'>
-            <button>
-                <FontAwesomeIcon icon={faUserPlus} className='follower-user-icon'/>
-            </button>
         </div>
-    </div>
-  );
+    );
 }
 
 export default PeopleYouMayKnow;
